aaabdb90a7cea8f8766ea0cfd70ef09a16095916
Terry LIn <terry1997116@gmail.com>
2019-11-11 17:10:40 +0800

    更新

--- color.js
+++ color.js
@@ -14,7 +14,7 @@ module.exports = {
             }
             client
                 .query(
-                    'SELECT color FROM public.iphone_colors',
+                    'SELECT color FROM public.colors',
                     function(err, result) {
                         if (err) {
                             console.log(err);
