f27e49724c3c51e6dd7606f18df79f5ab6966e73
Terry LIn <terry1997116@gmail.com>
2019-11-16 21:13:54 +0800

    更新

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
     PG_CONFIG: {
         user: process.env.PG_CONFIG_USER,
         database: process.env.PG_CONFIG_DATABASE,
-        password: process.env.PG_CONFIG_PASSWARD,
+        password: process.env.PG_CONFIG_PASSWORD,
         host: process.env.PG_CONFIG_HOST,
         port: 5432,
         max: 10,
