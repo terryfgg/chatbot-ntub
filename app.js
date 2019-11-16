8ef66ee12a8c2af51f00761b7eb5da9ad344ed6e
Terry LIn <terry1997116@gmail.com>
2019-11-16 23:00:16 +0800

    新增

--- app.js
+++ app.js
@@ -188,25 +188,42 @@ app.post('/webhook/', function (req, res) {
         data.entry.forEach(function (pageEntry) {
             var pageID = pageEntry.id;
             var timeOfEvent = pageEntry.time;
-
-            // Iterate over each messaging event
-            pageEntry.messaging.forEach(function (messagingEvent) {
-                if (messagingEvent.optin) {
-                    fbService.receivedAuthentication(messagingEvent);
-                } else if (messagingEvent.message) {
-                    receivedMessage(messagingEvent);
-                } else if (messagingEvent.delivery) {
-                    fbService.receivedDeliveryConfirmation(messagingEvent);
-                } else if (messagingEvent.postback) {
-                    receivedPostback(messagingEvent);
-                } else if (messagingEvent.read) {
-                    fbService.receivedMessageRead(messagingEvent);
-                } else if (messagingEvent.account_linking) {
-                    fbService.receivedAccountLink(messagingEvent);
-                } else {
-                    console.log("找不到匹配的 messagingEvent: ", messagingEvent);
+            // Secondary Receiver is in control - listen on standby channel
+            if (pageEntry.standby) {
+                    // iterate webhook events from standby channel
+                    pageEntry.standby.forEach(event => {
+                        const psid = event.sender.id;
+                        const message = event.message;
+                        console.log('message from: ', psid);
+                        console.log('message to inbox: ', message);
+                    });
                 }
-            });
+    
+                // Bot is in control - listen for messages
+                if (pageEntry.messaging) {
+                    // Iterate over each messaging event
+                    pageEntry.messaging.forEach(function (messagingEvent) {
+                        if (messagingEvent.optin) {
+                            fbService.receivedAuthentication(messagingEvent);
+                        } else if (messagingEvent.message) {
+                            receivedMessage(messagingEvent);
+                        } else if (messagingEvent.delivery) {
+                            fbService.receivedDeliveryConfirmation(messagingEvent);
+                        } else if (messagingEvent.postback) {
+                            receivedPostback(messagingEvent);
+                        } else if (messagingEvent.read) {
+                            fbService.receivedMessageRead(messagingEvent);
+                        } else if (messagingEvent.account_linking) {
+                            fbService.receivedAccountLink(messagingEvent);
+                        }else if (messagingEvent.pass_thread_control) {
+                            // do something with the metadata: messagingEvent.pass_thread_control.metadata
+                        } else {
+                            console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                        }
+                    });
+                }
+
+          
         });
 
         // 若上面都沒問題
@@ -307,6 +324,9 @@ function handleQuickReply(senderID, quickReply, messageId) {
 
 function handleDialogFlowAction(sender, action, messages, contexts, parameters) {
     switch (action) {
+        case "talk.human":
+            fbService.sendPassThread(sender);
+            break;
         case "unsubscribe":
             userService.newsletterSettings(function(updated) {
                 if (updated) {
